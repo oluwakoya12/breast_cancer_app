cb7a90bc94f68347409ffa83b33c7dd4313a4a78
oluwakoya12 <oluwakoyaisaaciyin@gmail.com>
2025-05-12 23:28:42 +0100

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -25,5 +26,7 @@ def predict():
         except Exception as e:
             return render_template('index.html', result=f'Error: {str(e)}')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
